8c887a986503d3816f4c2094836ba2fd394a2bbb
Tony Kowalewski <tony@kowalewski.dev>
2023-10-31 15:47:35 +0100

    add shuffle logic and summary

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 
+import quizCompleteImg from '../assets/quiz-complete.png';
 import questions from '../questions';
 
 const Quiz = () => {
   // we'll have array of questions and will track user answers and active question index
 
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length; //derived state
+  const activeQuestionIndex = userAnswers.length; //derived state - to know which question to show
+
+  const showQuiz = activeQuestionIndex < questions.length; //derived state - to know when questions are over
 
   console.log(userAnswers);
 
@@ -16,23 +19,35 @@ const Quiz = () => {
     });
   };
 
-  return (
-    <div id='quiz'>
-      <div id='question'>
-        <h3>{questions[activeQuestionIndex].text}</h3>
+  if (showQuiz) {
+    const shuffledAnswers = [...questions[activeQuestionIndex].answers];
+    shuffledAnswers.sort((a, b) => Math.random() - 0.5);
 
-        <ul id='answers'>
-          {questions[activeQuestionIndex].answers.map((answer, ind) => {
-            return (
-              <li key={ind} className='answer'>
-                <button onClick={selectAnswerHandler}>{answer}</button>
-              </li>
-            );
-          })}
-        </ul>
+    return (
+      <div id='quiz'>
+        <div id='question'>
+          <h3>{questions[activeQuestionIndex].text}</h3>
+
+          <ul id='answers'>
+            {shuffledAnswers.map((answer, ind) => {
+              return (
+                <li key={ind} className='answer'>
+                  <button onClick={selectAnswerHandler}>{answer}</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div id='summary'>
+        <img src={quizCompleteImg} alt='Trophy Icon' />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
 };
 
 export default Quiz;
