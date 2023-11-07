9fb798b3f72085bb0f3753c93dcea86f32573e52
Tony Kowalewski <tony@kowalewski.dev>
2023-11-07 03:20:54 +0100

    add highlight- selected answ

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,19 +5,38 @@ import questions from '../questions';
 import QuestionTimer from './QuestionTimer';
 
 const Quiz = () => {
+  const [answerState, setAnswerState] = useState('');
   // we'll have array of questions and will track user answers and active question index
 
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length; //derived state - to know which question to show
+  const activeQuestionIndex =
+    answerState === '' ? userAnswers.length : userAnswers.length - 1; //derived state - to know which question to show
 
   const showQuiz = activeQuestionIndex < questions.length; //derived state - to know when questions are over
 
   console.log(userAnswers);
 
   const selectAnswerHandler = (e) => {
+    const selectedAnswer = e.target.textContent;
+
+    setAnswerState('answered');
+
     setUserAnswers((prevState) => {
-      return [...prevState, e.target.textContent];
+      return [...prevState, selectedAnswer];
     });
+
+    setTimeout(() => {
+      if (questions[activeQuestionIndex].answers[0] === selectedAnswer) {
+        setAnswerState('correct');
+      } else {
+        setAnswerState('wrong');
+      }
+
+      // Resetting answer
+      setTimeout(() => {
+        setAnswerState('');
+      }, 2000);
+    }, 1000);
   };
 
   const skipAnswerHandler = useCallback(() => {
@@ -35,19 +54,35 @@ const Quiz = () => {
 
           <ul id='answers'>
             {shuffledAnswers.map((answer, ind) => {
+              let answerClass = '';
+              const isSelected = answer === userAnswers[userAnswers.length - 1];
+
+              if (isSelected && answerState === 'answered') {
+                answerClass = 'selected';
+              }
+
+              if (
+                isSelected &&
+                (answerState === 'correct' || answerState === 'wrong')
+              ) {
+                answerClass = answerState;
+              }
+
               return (
                 <li key={ind} className='answer'>
-                  <button onClick={selectAnswerHandler}>{answer}</button>
+                  <button className={answerClass} onClick={selectAnswerHandler}>
+                    {answer}
+                  </button>
                 </li>
               );
             })}
           </ul>
 
-          <QuestionTimer
+          {/* <QuestionTimer
             key={activeQuestionIndex}
             time={10000}
             onTimeout={skipAnswerHandler}
-          />
+          /> */}
         </div>
       </div>
     );
